f2fa65883e5c51e36ac32cd768c84f75942c4446
Jayson Tai <jayson.tai@hotmail.com>
2021-02-24 02:50:46 +1300

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log("making sure that this works");
---
+++ dist/something.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function something() {
+    return "asdasdasdsa";
+}
+exports.default = something;
